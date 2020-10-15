e64a43c399deae1d04ab31f58799d40a853a0a68
Shubham Wazir <SelfProclaimedDev@gmail.com>
2020-10-15 22:18:36 +0530

    Second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,11 @@ import {ThemeProvider} from '@material-ui/styles';
 import theme from './ui/Theme';
 import Header from './ui/Header';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import Footer from '../components/ui/Footer';
 import LandingPage from './LandingPage';
+import Services from './Services';
+import CustomSoftware from './CustomSoftware';
 
 function App() {
 
@@ -15,9 +18,15 @@ function App() {
       <BrowserRouter>
         <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
         <Switch>
-          <Route exact path='/' render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex}/>} />
-          <Route exact path='/services' component={() => <div>Services</div>} />
-          <Route exact path='/customsoftware' component={() => <div>Custom Software</div>} />
+          <Route exact path='/' 
+          render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex}/>} 
+          />
+
+          <Route exact path='/services' 
+          render={(props) => <Services {...props} setValue={setValue} setSelectedIndex={setSelectedIndex}/>} 
+          />
+
+          <Route exact path='/customsoftware' render={(props) => <CustomSoftware {...props} setValue={setValue} setSelectedIndex={setSelectedIndex}/>}  />
           <Route exact path='/mobileapps' component={() => <div>Mobile Apps</div>} />
           <Route exact path='/websites' component={() => <div>Website</div>} />
           <Route exact path='/revolution' component={() => <div>Revolution</div>} />
